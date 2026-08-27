72cc292d2aa756c4a1d2265026107642e1b89e26
ionnuthub <raducugionut@gmail.com>
2026-08-27 13:03:36 +0200

    Creare feature:interceptor/logger

---
+++ src/common/filters/all-exceptions/all-exceptions.filter.ts
@@ -0,0 +1,42 @@
+import {
+  ArgumentsHost,
+  Catch,
+  ExceptionFilter,
+  HttpException,
+  HttpStatus,
+} from '@nestjs/common';
+import { Request, Response } from 'express';
+
+@Catch()
+export class AllExceptionsFilter implements ExceptionFilter {
+  catch(exception: unknown, host: ArgumentsHost) {
+    const context = host.switchToHttp();
+
+    const response = context.getResponse<Response>();
+    const request = context.getRequest<Request>();
+
+    const isHttpException = exception instanceof HttpException;
+
+    const status = isHttpException
+        ? exception.getStatus()
+        : HttpStatus.INTERNAL_SERVER_ERROR;
+
+    const exceptionResponse = isHttpException
+        ? exception.getResponse()
+        : null;
+
+    const message =
+        typeof exceptionResponse === 'object' &&
+        exceptionResponse !== null &&
+        'message' in exceptionResponse
+            ? exceptionResponse.message
+            : 'A apărut o eroare internă';
+
+    response.status(status).json({
+      statusCode: status,
+      message,
+      path: request.url,
+      timestamp: new Date().toISOString(),
+    });
+  }
+}
