af7c6b4509654a3d02835395b9978ebb1ee7a07c
ionnuthub <raducugionut@gmail.com>
2026-09-01 14:52:59 +0200

    Add Swagger API documentation

--- src/auth/dto/login.dto/login.dto.ts
+++ src/auth/dto/login.dto/login.dto.ts
@@ -1,9 +1,19 @@
 import {IsEmail, IsString} from 'class-validator';
+import {ApiProperty} from "@nestjs/swagger";
+
 
 export class LoginDto {
+    @ApiProperty({
+        description: 'Adresa de email a utilizatorului',
+        example: 'user@example.com'
+    })
     @IsEmail()
     email: string;
 
+    @ApiProperty({
+        description: 'Parola utilizatorului',
+        example: 'parolaMeaSecurizata123'
+    })
     @IsString()
     password: string;
 }
